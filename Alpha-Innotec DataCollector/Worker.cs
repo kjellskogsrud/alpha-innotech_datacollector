2cc3f72ffaaa3cc5ddfc94da81ff2c722d2154d8
Kjell Skogsrud <kjell@skogsrud.net>
2021-10-25 13:25:34 +0200

    Update default VS template code to StyleCop
    Update the standard code from the template to confirm to StyleCop
    standards for documentation.

--- Alpha-Innotec DataCollector/Worker.cs
+++ Alpha-Innotec DataCollector/Worker.cs
@@ -1,27 +1,35 @@
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+// <copyright file="Worker.cs" company="Kjell Skogsrud">
+// Copyright (c) Kjell Skogsrud. BSD 3-Clause License
+// </copyright>
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Alpha_Innotec_DataCollector
 {
+    /// <inheritdoc/>
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
+        private readonly ILogger<Worker> logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Worker"/> class.
+        /// </summary>
+        /// <param name="logger">Pass in the logger.</param>
         public Worker(ILogger<Worker> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
+        /// <inheritdoc/>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                this.logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
